673d581a394a5b2a4afef6c93c9f72669aec7ded
NailikGit <kilian.smeets@web.de>
2025-08-15 22:02:36 +0200

    make code nicer to look at

--- qoic.c
+++ qoic.c
@@ -7,47 +7,26 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdbool.h>
 
-int memfd_create(const char* name, unsigned int flags);
-
-struct image { // raw image data
+typedef struct { // raw image data
   uint32_t width;
   uint32_t height;
   uint8_t channels;
   uint8_t colorspace;
   uint8_t** data;
-};
+} image;
 
-struct pixel {
+typedef struct {
   uint8_t r;
   uint8_t g;
   uint8_t b;
   uint8_t a;
-};
+} pixel;
 
-struct pixel array[64] = {0};
+pixel array[64] = {0};
 
-_Bool peq(struct pixel a, struct pixel b) {
-  return a.r == b.r && a.g == b.g && a.b == b.b && a.a == b.a;
-}
-
-_Bool pdiff(int8_t dr, int8_t dg, int8_t db) {
-  return -2 <= dr && dr <= 1
-      && -2 <= dg && dg <= 1
-      && -2 <= db && db <= 1;
-}
-
-_Bool pldiff(int8_t dr_dg, int8_t dg, int8_t db_dg) {
-  return - 8 <= dr_dg && dr_dg <=  7
-      && -32 <=    dg &&    dg <= 31
-      && - 8 <= db_dg && db_dg <=  7;
-}
-
-uint8_t hash(struct pixel p) {
-  return (p.r * 3 + p.g * 5 + p.b * 7 + p.a * 11) % 64;
-}
-
-void write_raw(struct image img, char* outfile) {
+void write_raw(image img, char* outfile) {
   FILE* output = fopen(outfile, "w");
   if(!output) {
     fprintf(stderr, "ERROR: Couldn't open file \"%s\"\n", outfile);
@@ -60,7 +39,7 @@ void write_raw(struct image img, char* outfile) {
   fclose(output);
 }
 
-void write_png(struct image img, char* outfile) {
+void write_png(image img, char* outfile) {
   FILE* output = fopen(outfile, "w");
   if(!output) {
     fprintf(stderr, "ERROR: Couldn't open file \"%s\"\n", outfile);
@@ -103,8 +82,28 @@ void write_png(struct image img, char* outfile) {
   png_destroy_write_struct(&png_ptr, &info_ptr);
 }
 
-void write_qoi(struct image img, char* outfile) {
-  memset(array, 0, sizeof(struct pixel) * 64);
+bool pixel_equal(pixel a, pixel b) {
+  return a.r == b.r && a.g == b.g && a.b == b.b && a.a == b.a;
+}
+
+bool pixel_diff(int8_t dr, int8_t dg, int8_t db) {
+  return -2 <= dr && dr <= 1
+      && -2 <= dg && dg <= 1
+      && -2 <= db && db <= 1;
+}
+
+bool pixel_diff_luma(int8_t dr_dg, int8_t dg, int8_t db_dg) {
+  return - 8 <= dr_dg && dr_dg <=  7
+      && -32 <=    dg &&    dg <= 31
+      && - 8 <= db_dg && db_dg <=  7;
+}
+
+uint8_t hash(pixel p) {
+  return (p.r * 3 + p.g * 5 + p.b * 7 + p.a * 11) % 64;
+}
+
+void write_qoi(image img, char* outfile) {
+  memset(array, 0, sizeof(pixel) * 64);
 
   FILE* output = fopen(outfile, "w");
   if(!output) {
@@ -126,10 +125,10 @@ void write_qoi(struct image img, char* outfile) {
   fwrite(&img.channels, 1, 1, output);
   fwrite(&img.colorspace, 1, 1, output);
 
-  struct pixel pp = {.a = 255};
-  struct pixel cp;
+  pixel pp = {.a = 255};
+  pixel cp;
 
-  uint8_t ph;
+  uint8_t pixel_hash;
   int8_t dr, dg, db, dr_dg, db_dg;
   for(int i = 0; i < img.height; i++) {
     for(int j = 0; j < img.width; j++) {
@@ -138,8 +137,8 @@ void write_qoi(struct image img, char* outfile) {
       cp.b = img.data[i][j * img.channels + 2];
       if(img.channels == 4) cp.a = img.data[i][j * img.channels + 3];
 
-      if(peq(cp, pp)) { // qoi_run
-        _Bool b = 0;
+      if(pixel_equal(cp, pp)) { // qoi_run
+        bool b = false;
         uint8_t k = 1;
 
         for(; k < 62; k++) {
@@ -148,7 +147,7 @@ void write_qoi(struct image img, char* outfile) {
           cp.g = img.data[i][(j + k) * img.channels + 1];
           cp.b = img.data[i][(j + k) * img.channels + 2];
           if(img.channels == 4) cp.a = img.data[i][(j + k) * img.channels + 3];
-          if(!peq(cp, pp)) {j++; b = 1; break;}
+          if(!pixel_equal(cp, pp)) {j++; b = true; break;}
         }
 
         j += k - 1;
@@ -159,33 +158,43 @@ void write_qoi(struct image img, char* outfile) {
 
         if(!b) continue;
       }
-      if(peq(array[ph = hash(cp)], cp)) { // qoi_index
-        fwrite(&ph, 1, 1, output);
+
+      if(pixel_equal(array[pixel_hash = hash(cp)], cp)) { // qoi_index
+        fwrite(&pixel_hash, 1, 1, output);
+
       } else if(cp.a != pp.a) { // qoi_rgba
         uint8_t rgba = -1;
         fwrite(&rgba, 1, 1, output);
         fwrite(&cp, 1, 4, output);
-      } else if(pdiff(dr = cp.r - pp.r, dg = cp.g - pp.g, db = cp.b - pp.b)) { // qoi_diff
+
+      } else if(pixel_diff(dr = cp.r - pp.r, // qoi_diff
+                           dg = cp.g - pp.g,
+                           db = cp.b - pp.b)) {
         uint8_t diff = (0b01 << 6) | ((dr + 2) << 4) | ((dg + 2) << 2) | (db + 2);
         fwrite(&diff, 1, 1, output);
-      } else if(pldiff(dr_dg = dr - dg, dg, db_dg = db - dg)) { // qoi_diff_luma
+
+      } else if(pixel_diff_luma(dr_dg = dr - dg, // qoi_diff_luma
+                                dg,
+                                db_dg = db - dg)) {
         uint8_t luma[2] = {(0b10 << 6) | (dg + 32), (dr_dg + 8) << 4 | (db_dg + 8)};
         fwrite(luma, 1, 2, output);
+
       } else { // qoi_rgb
         uint8_t rgb = -2;
         fwrite(&rgb, 1, 1, output);
         fwrite(&cp, 1, 3, output);
       }
-      array[ph] = cp;
+
+      array[pixel_hash] = cp;
       pp = cp;
     }
   }
 
-  fwrite("\0\0\0\0\0\0\0\1", 1, 8, output);
+  fwrite("\0\0\0\0\0\0\0\1", 1, 8, output); // qoi end tag
 }
 
-struct image read_raw(char* infile, uint32_t width, uint32_t height, uint8_t channels) {
-  struct image r = {0};
+image read_raw(char* infile, uint32_t width, uint32_t height, uint8_t channels) {
+  image r = {0};
 
   FILE* file = fopen(infile, "r");
   if(!file) {
@@ -202,21 +211,21 @@ struct image read_raw(char* infile, uint32_t width, uint32_t height, uint8_t cha
   return r;
 }
 
-struct image read_png(char* infile) {
+image read_png(char* infile) {
   int number = 4;
   uint8_t* header = malloc(number);
 
   FILE* file = fopen(infile, "rb");
   if(!file) {
     fprintf(stderr, "ERROR: No such file: \"%s\"\n", infile);
-    return (struct image) {0};
+    return (image) {0};
   }
 
   if(fread(header, 1, number, file) != number) {
     fprintf(stderr, "ERROR: Couldn't read from file \"%s\"\n", infile);
     free(header);
     fclose(file);
-    return (struct image) {0};
+    return (image) {0};
   }
 
   int is_png = !png_sig_cmp(header, 0, number);
@@ -224,7 +233,7 @@ struct image read_png(char* infile) {
     fprintf(stderr, "ERROR: File \"%s\" is not a png image\n", infile);
     free(header);
     fclose(file);
-    return (struct image) {0};
+    return (image) {0};
   }
 
   free(header);
@@ -234,7 +243,7 @@ struct image read_png(char* infile) {
   if(!png_ptr) {
     fprintf(stderr, "ERROR: Couldn't create png_ptr\n");
     fclose(file);
-    return (struct image) {0};
+    return (image) {0};
   }
 
   png_infop info_ptr = png_create_info_struct(png_ptr);
@@ -243,14 +252,14 @@ struct image read_png(char* infile) {
     fprintf(stderr, "ERROR: Couldn't create info_ptr\n");
     png_destroy_read_struct(&png_ptr, NULL, NULL);
     fclose(file);
-    return (struct image) {0};
+    return (image) {0};
   }
 
   if(setjmp(png_jmpbuf(png_ptr))) {
     fprintf(stderr, "ERROR: An error occurred\n");
     png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
     fclose(file);
-    return (struct image) {0};
+    return (image) {0};
   }
 
   png_init_io(png_ptr, file);
@@ -259,7 +268,7 @@ struct image read_png(char* infile) {
   png_read_png(png_ptr, info_ptr, 0, NULL);
   fclose(file);
   
-  struct image r = {0};
+  image r = {0};
 
   uint8_t b = png_get_bit_depth(png_ptr, info_ptr);
   if(b != 8) {
@@ -267,14 +276,14 @@ struct image read_png(char* infile) {
         infile, b
     );
     png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-    return (struct image) {0};
+    return (image) {0};
   }
 
   r.width = png_get_image_width(png_ptr, info_ptr);
   r.height = png_get_image_height(png_ptr, info_ptr);
   uint8_t c = png_get_color_type(png_ptr, info_ptr);
-  if(c == 2) c = 3;
-  if(c == 6) c = 4;
+  if(c == 2) c = 3; // 2 == rgb
+  if(c == 6) c = 4; // 6 == rgba
   r.channels = c;
 
   uint8_t** rows = png_get_rows(png_ptr, info_ptr);
@@ -312,10 +321,10 @@ int qoi_end(uint8_t* read_buf) {
   return 1;
 }
 
-struct image read_qoi(char* infile) {
-  memset(array, 0, sizeof(struct pixel) * 64);
+image read_qoi(char* infile) {
+  memset(array, 0, sizeof(pixel) * 64);
 
-  struct image img = {0};
+  image img = {0};
 
   FILE* file = fopen(infile, "r");
   if(!file) {
@@ -353,28 +362,32 @@ struct image read_qoi(char* infile) {
   uint8_t read_buf[8] = {0};
   fread(read_buf, 1, 8, file);
 
-  struct pixel pp = {.a = 255};
-  struct pixel cp;
+  pixel pp = {.a = 255};
+  pixel cp;
 
-  uint8_t ph;
   int8_t dr, dg, db, dr_dg, db_dg;
   for(int i = 0; i < img.height; i++) {
     for(int j = 0; j < img.width * img.channels; j++) {
       if(qoi_end(read_buf)) goto end;
+
       if(read_buf[0] == (uint8_t) -1) { // qoi_rgba
         img.data[i][j] = (cp.r = read_buf[1]);
         img.data[i][j + 1] = (cp.g = read_buf[2]);
         img.data[i][j + 2] = (cp.b = read_buf[3]);
         img.data[i][j + 3] = (cp.a = read_buf[4]);
+
         shift_read(read_buf, 4, file);
         j += 3;
+
       } else if(read_buf[0] == (uint8_t) -2) { // qoi_rgb
         img.data[i][j] = (cp.r = read_buf[1]);
         img.data[i][j + 1] = (cp.g = read_buf[2]);
         img.data[i][j + 2] = (cp.b = read_buf[3]);
         if(img.channels == 4) img.data[i][j + 3] = cp.a;
+
         shift_read(read_buf, 3, file);
         j += img.channels - 1;
+
       } else if((read_buf[0] >> 6) == 0b00) { // qoi_index
         cp = array[read_buf[0]];
 
@@ -384,27 +397,34 @@ struct image read_qoi(char* infile) {
         if(img.channels == 4) img.data[i][j + 3] = cp.a;
 
         j += img.channels - 1;
+
       } else if((read_buf[0] >> 6) == 0b01) { // qoi_diff
         dr = ((read_buf[0] >> 4) & 0b11) - 2;
         dg = ((read_buf[0] >> 2) & 0b11) - 2;
         db = (read_buf[0] & 0b11) - 2;
+
         img.data[i][j] = (cp.r += dr);
         img.data[i][j + 1] = (cp.g += dg);
         img.data[i][j + 2] = (cp.b += db);
         if(img.channels == 4) img.data[i][j + 3] = cp.a;
+
         j += img.channels - 1;
+
       } else if((read_buf[0] >> 6) == 0b10) { // qoi_diff_luma
         dg = (read_buf[0] & 0b111111) - 32;
-        shift_read1(read_buf, file);
-        db_dg = (read_buf[0] & 0b1111) - 8;
-        dr_dg = (read_buf[0] >> 4) - 8;
+        db_dg = (read_buf[1] & 0b1111) - 8;
+        dr_dg = (read_buf[1] >> 4) - 8;
         dr = dr_dg + dg;
         db = db_dg + dg;
+
         img.data[i][j] = (cp.r += dr);
         img.data[i][j + 1] = (cp.g += dg);
         img.data[i][j + 2] = (cp.b += db);
         if(img.channels == 4) img.data[i][j + 3] = cp.a;
+
+        shift_read1(read_buf, file);
         j += img.channels - 1;
+
       } else if((read_buf[0] >> 6) == 0b11) { // qoi_run
         uint8_t run = (read_buf[0] & 0b111111) + 1;
         for(int k = 0; k < run; k++) {
@@ -448,38 +468,40 @@ void usage() {
 int main(int argc, char** argv) {
   uint32_t width = 0, height = 0;
   uint32_t channels = 3, colorspace = 0;
-  _Bool ws = 0, hs = 0, cs = 0, ss = 0;
-  struct image img = {0};
+  bool ws = false, hs = false, cs = false, ss = false;
+
+  image img = {0};
+
   int i;
   for(i = 1; i < argc; i++) {
     if(argv[i][0] != '-') break;
     if(strcmp("-?", argv[i]) == 0 || strcmp("--help", argv[i]) == 0) {
       usage();
       return 0;
-    }
-    if(strcmp("-w", argv[i]) == 0) {
+
+    } else if(strcmp("-w", argv[i]) == 0) {
       if(++i >= argc) goto fail;
       int ret = sscanf(argv[i], "%d", &width);
       if(ret != 1) goto fail;
-      ws = 1;
-    }
-    if(strcmp("-h", argv[i]) == 0) {
+      ws = true;
+
+    } else if(strcmp("-h", argv[i]) == 0) {
       if(++i >= argc) goto fail;
       int ret = sscanf(argv[i], "%d", &height);
       if(ret != 1) goto fail;
-      hs = 1;
-    }
-    if(strcmp("-c", argv[i]) == 0) {
+      hs = true;
+
+    } else if(strcmp("-c", argv[i]) == 0) {
       if(++i >= argc) goto fail;
       int ret = sscanf(argv[i], "%d", &channels);
       if(ret != 1) goto fail;
-      cs = 1;
-    }
-    if(strcmp("-s", argv[i]) == 0) {
+      cs = true;
+
+    } else if(strcmp("-s", argv[i]) == 0) {
       if(++i >= argc) goto fail;
       int ret = sscanf(argv[i], "%d", &colorspace);
       if(ret != 1) goto fail;
-      ss = 1;
+      ss = true;
     }
   }
 
@@ -491,15 +513,18 @@ int main(int argc, char** argv) {
   if(strcmp("raw", argv[i]) == 0) {
     if(width == 0 || height == 0) goto fail;
     img = read_raw(argv[i + 1], width, height, channels);
+
   } else if(strcmp("png", argv[i]) == 0) {
     img = read_png(argv[i + 1]);
+
   } else if(strcmp("qoi", argv[i]) == 0) {
     img = read_qoi(argv[i + 1]);
+
   } else {
     goto fail;
   }
 
-  if(!img.data) return 1;
+  if(!img.data) goto fail;
 
   img.width = ws ? width : img.width;
   img.height = hs ? height : img.height;
@@ -508,11 +533,18 @@ int main(int argc, char** argv) {
 
   if(strcmp("raw", argv[i + 2]) == 0) {
     write_raw(img, argv[i + 3]);
+
   } else if(strcmp("png", argv[i + 2]) == 0) {
     write_png(img, argv[i + 3]);
+
   } else if(strcmp("qoi", argv[i + 2]) == 0) {
     write_qoi(img, argv[i + 3]);
+
   } else {
+    for(int i = 0; i < img.height; i++) {
+      free(img.data[i]);
+    }
+    free(img.data);
     goto fail;
   }
 
@@ -524,6 +556,5 @@ int main(int argc, char** argv) {
 
   fail:
   usage();
-  free(img.data);
   return 1;
 }
